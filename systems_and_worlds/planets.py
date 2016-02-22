6d38cd8660cec96ac3aaf4b98ada20009fc877f5
jkaz1968 <jkaz1968@gmail.com>
2016-02-21 23:55:45 -0600

    new file structure

--- systems_and_worlds/planets.py
+++ systems_and_worlds/planets.py
@@ -12,6 +12,7 @@
 
 import random
 
+
 def intro():
     print("Sector Generator 1.0")
     print("A script for generating random sectors in Classic Traveller")
@@ -30,51 +31,66 @@ def intro():
     print("with worlds to explore, without all the tedium of dice and math.")
     return
 
+
 def get_sector_info():
     density = []
-    for ss in range(0,16):
-        print("Enter the system density of subsector ["+chr(ss+65)+"]: ")
+    for ss in range(0, 16):
+        print("Enter the system density of subsector [" + chr(ss + 65) + "]: ")
         density.append(int(input()))
     return density
+
 
 def gen_upp():
     upp = []
     # starport type
     roll = roll_dice()
-    if roll == 12: upp.append('X')
-    elif roll == 11 or roll == 10: upp.append('E')
-    elif roll == 9: upp.append('D')
-    elif roll == 8 or roll == 7: upp.append('C')
-    elif roll == 6 or roll == 5: upp.append('B')
-    elif roll <= 4: upp.append('A')
+    if roll == 12:
+        upp.append('X')
+    elif roll == 11 or roll == 10:
+        upp.append('E')
+    elif roll == 9:
+        upp.append('D')
+    elif roll == 8 or roll == 7:
+        upp.append('C')
+    elif roll == 6 or roll == 5:
+        upp.append('B')
+    elif roll <= 4:
+        upp.append('A')
     # size
     size = roll_dice()
-    if size < 0: size = 0
+    if size < 0:
+        size = 0
     upp.append(size)
     # atmosphere
     atmo = roll_dice() - 7 + size
-    if atmo < 0: atmo = 0
+    if atmo < 0:
+        atmo = 0
     upp.append(atmo)
     # hydrographics
     hydro = roll_dice() - 7 + atmo
-    if hydro < 0: hydro = 0
+    if hydro < 0:
+        hydro = 0
     upp.append(hydro)
     # population
     ppl = roll_dice() - 2
-    if ppl < 0: ppl = 0
+    if ppl < 0:
+        ppl = 0
     upp.append(ppl)
     # government
     govt = roll_dice() - 7 + ppl
-    if govt < 0: govt=0
+    if govt < 0:
+        govt = 0
     upp.append(govt)
     # law level
     law = roll_dice() - 7 + govt
-    if law < 0: law=0
+    if law < 0:
+        law = 0
     upp.append(law)
     # tech level
     tech = get_tech_level(upp)
     upp.append(tech)
     return upp
+
 
 def get_tech_level(upp):
     tl = 0
@@ -90,7 +106,7 @@ def get_tech_level(upp):
     # from size
     if upp[1] <= 1:
         tl += 2
-    elif upp[1] <= 4 and upp[1] >= 2:
+    elif 4 >= upp[1] >= 2:
         tl += 1
     # from atm
     if upp[2] <= 3:
@@ -108,17 +124,18 @@ def get_tech_level(upp):
     if upp[4] == 9:
         tl += 2
     if upp[4] == 10:
-        tl +4
+        tl += 4
     # from govt
     if upp[5] == 0 or upp[4] == 5:
         tl += 1
     if upp[5] == 13:
         tl -= 2
-    if tl < 0: tl = 0
-    return convert_to_hexish(tl + random.randint(0,7))
-    
+    if tl < 0:
+        tl = 0
+    return convert_to_hexish(tl + random.randint(0, 7))
+
+
 def gen_bases(upp):
-    bases = ''
     n = False
     s = False
     dm = 0
@@ -127,36 +144,37 @@ def gen_bases(upp):
         if roll_dice() >= 8:
             n = True
     # scout
-    if upp[0] == 'C': dm -= 1
-    elif upp[0] == 'B': dm -= 2
-    elif upp[0] == 'A': dm -= 3
-    if roll_dice() + dm >= 7: s = True
-    if n==True and s==False:
+    if upp[0] == 'C':
+        dm -= 1
+    elif upp[0] == 'B':
+        dm -= 2
+    elif upp[0] == 'A':
+        dm -= 3
+    if roll_dice() + dm >= 7:
+        s = True
+    if n is True and s is False:
         return 'N'
-    elif s==True and n==False:
+    elif s is True and n is False:
         return 'S'
-    elif s==True and n==True:
+    elif s is True and n is True:
         return 'A'
     else:
         return ' '
 
+
 def gen_notes(upp):
     notes = ''
-    if upp[2] >=4 and upp[2] <= 9:
-        if upp[3] >= 4 and upp[3] <= 8:
-            if upp[4] >= 5 and upp[4] <= 7:
-                notes += 'Agricultural. '
+    if 4 <= upp[2] <= 9 and 4 <= upp[3] <= 8 and 5 <= upp[4] <= 7:
+        notes += 'Agricultural. '
     if upp[2] <= 3 and upp[3] <= 3 and upp[4] >= 6:
         notes += 'Non-Agricultural. '
     if (upp[2] >= 2 or upp[2] == 4 or upp[2] == 7 or upp[2] == 9) and upp[4] >= 9:
         notes += 'Industrial. '
     if upp[4] <= 6:
         notes += 'Non-Industrial. '
-    if upp[2] == 6 or upp[2] == 8:
-        if upp[4] >= 6 and upp[4] <= 8:
-            if upp[5] >= 4 and upp[5] <= 9:
-                notes += 'Rich. '
-    if (upp[2] <= 2 and upp[2] >= 5) and upp[3] <= 3:
+    if (upp[2] == 6 or upp[2] == 8) and 6 <= upp[4] <= 8 and 4 <= upp[5] <= 9:
+        notes += 'Rich. '
+    if (2 >= upp[2] >= 5) and upp[3] <= 3:
         notes += 'Poor. '
     if upp[3] == 10:
         notes += 'Water World. '
@@ -166,32 +184,37 @@ def gen_notes(upp):
         notes += 'Vacuum World. '
     if upp[1] == 0:
         notes += 'Asteroid. '
-    if (upp[2] <= 1 and upp[2] > 0) and upp[3] >= 1:
+    if (1 >= upp[2] > 0) and upp[3] >= 1:
         notes += 'Ice Capped. '
     return notes
+
 
 def gen_zone():
     return
 
+
 def gen_gas():
     gas = ' '
-    if roll_dice() <= 10: gas = 'G'
+    if roll_dice() <= 10:
+        gas = 'G'
     return gas
 
+
 def roll_dice():
-    return random.randint(1,6) + random.randint(1,6)
+    return random.randint(1, 6) + random.randint(1, 6)
+
 
 def convert_upp_to_string(upp):
     upp_string = ''
-    upp_string += upp[0]                    # starport
-    upp_string += str(convert_to_hexish(upp[1])) # size
-    upp_string += str(convert_to_hexish(upp[2])) # atmosphere
-    upp_string += str(convert_to_hexish(upp[3])) # hydrographics
-    upp_string += str(convert_to_hexish(upp[4])) # population
-    upp_string += str(convert_to_hexish(upp[5])) # govt
-    upp_string += str(convert_to_hexish(upp[6])) # law
+    upp_string += upp[0]  # starport
+    upp_string += str(convert_to_hexish(upp[1]))  # size
+    upp_string += str(convert_to_hexish(upp[2]))  # atmosphere
+    upp_string += str(convert_to_hexish(upp[3]))  # hydrographics
+    upp_string += str(convert_to_hexish(upp[4]))  # population
+    upp_string += str(convert_to_hexish(upp[5]))  # govt
+    upp_string += str(convert_to_hexish(upp[6]))  # law
     upp_string += "-"
-    upp_string += str(upp[7]) # tech level
+    upp_string += str(upp[7])  # tech level
     return upp_string
 
 
@@ -210,7 +233,7 @@ def convert_to_hexish(value):
         return 'F'
     else:
         return value
-    return value
+
 
 def format_world_data(name, location, upp, base, notes, gas):
     world_string = ''
@@ -221,6 +244,7 @@ def format_world_data(name, location, upp, base, notes, gas):
     world_string += (' ' + notes + (' ' * (50 - len(notes))))
     world_string += (' ' + gas)
     return world_string
+
 
 def get_density():
     density = 0
@@ -234,17 +258,18 @@ def get_density():
         density = int(input("Select 1-5: "))
     return density
 
+
 def gen_sector(density):
-    for ss in range(0,16):
-        print("\nSubsector ["+chr(ss+65)+"]")
-        for c in range(1,9):
-            for r in range(1,11):
-                if random.randint(1,6) <= density[ss]:
+    for ss in range(0, 16):
+        print("\nSubsector [" + chr(ss + 65) + "]")
+        for c in range(1, 9):
+            for r in range(1, 11):
+                if random.randint(1, 6) <= density[ss]:
                     if r < 10:
                         location = '0' + str(c) + '0' + str(r)
                     else:
                         location = '0' + str(c) + str(r)
-                    name = 'World-'+location
+                    name = 'World-' + location
                     upp = gen_upp()
                     upp_str = convert_upp_to_string(upp)
                     bases = gen_bases(upp)
@@ -254,9 +279,11 @@ def gen_sector(density):
                     print(upp_formatted)
     return
 
+
 def main():
     intro()
     density = get_sector_info()
     gen_sector(density)
+
 
 main()
