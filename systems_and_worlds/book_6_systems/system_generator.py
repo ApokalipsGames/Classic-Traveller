81bc874c14b4e1b82b1f6558866a7ba92054898a
jkaz1968 <jkaz1968@gmail.com>
2016-02-22 22:43:03 -0600

    book 6
    added startypes.py and shell of system_generator.py

---
+++ systems_and_worlds/book_6_systems/system_generator.py
@@ -0,0 +1,17 @@
+# system_generator.py
+# book 6 - Scouts
+# enhanced star system generation
+
+import random
+import math
+
+
+def intro():
+    return
+
+
+def roll_two_dice():
+    return random.randint(1,6) + random.randint(1,6)
+
+
+
